0e16371f3e6ecf6195ff2981a6ad7d728947cf7e
TimeConqueror <timeconqueror999@gmail.com>
2022-05-29 01:14:25 +0300

    Fixed cube shape

--- Engine/Graphics/Shape.cpp
+++ Engine/Graphics/Shape.cpp
@@ -19,14 +19,14 @@ Shape Shape::quad(float width, float height) {
 Shape Shape::cube(float width, float height, float depth) {
 	return Shape(std::vector(
 		             {
-			             Vertex(0, 0, 0, 0.0F, 1.0F, 1.0F, 1.0F),
-			             Vertex(width, 0, 0, 0.0F, 1.0F, 1.0F, 1.0F),
-			             Vertex(width, 0, depth, 0.0F, 1.0F, 1.0F, 1.0F),
-			             Vertex(0, 0, depth, 0.0F, 1.0F, 1.0F, 1.0F),
-			             Vertex(0, height, 0, 0.0F, 1.0F, 1.0F, 1.0F),
-			             Vertex(width, height, 0, 0.0F, 1.0F, 1.0F, 1.0F),
-			             Vertex(width, height, depth, 0.0F, 1.0F, 1.0F, 1.0F),
-			             Vertex(0, height, depth, 0.0F, 1.0F, 1.0F, 1.0F),
+			             Vertex(0, 0, 0, 0.5F, 0.0F, 1.0F, 1.0F),
+			             Vertex(width, 0, 0, 0.0F, 0.0F, 1.0F, 1.0F),
+			             Vertex(width, 0, depth, 1.0F, 0.0F, 1.0F, 1.0F),
+			             Vertex(0, 0, depth, 0.0F, 1.0F, 0.0F, 1.0F),
+			             Vertex(0, height, 0, 0.0F, 1.0F, 0.0F, 1.0F),
+			             Vertex(width, height, 0, 0.0F, 0.0F, 1.0F, 1.0F),
+			             Vertex(width, height, depth, 0.0F, 0.0F, 1.0F, 1.0F),
+			             Vertex(0, height, depth, 1.0F, 1.0F, 1.0F, 1.0F),
 		             }),
 	             std::vector<uint>(
 		             {
@@ -35,7 +35,7 @@ Shape Shape::cube(float width, float height, float depth) {
 			             1, 2, 6, 1, 6, 5, //x+
 			             0, 3, 7, 0, 7, 4, //x-
 			             2, 3, 7, 2, 7, 6, //z+
-			             1, 0, 4, 1, 4, 3 //z-
+			             1, 0, 4, 1, 5, 4 //z-
 		             }));
 }
 
